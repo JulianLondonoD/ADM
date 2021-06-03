38b6dbe5dc24a6d8e0735c9d85d555fe65df69c6
JulianLondonoD <52910906+JulianLondonoD@users.noreply.github.com>
2021-06-03 17:39:07 +0200

    Back-End .Net Core

--- ADM.PruebaTecnica/ADM.PruebaTecnica.Services/Domain/AlgortimosDomain.cs
+++ ADM.PruebaTecnica/ADM.PruebaTecnica.Services/Domain/AlgortimosDomain.cs
@@ -10,28 +10,42 @@ namespace ADM.PruebaTecnica.Services.Domain
         public double MultiplicarNumeroSinMultiplicacion(double numeroA, double numeroB)
         {
             double acum = 0;
-            if (numeroA > numeroB)
+            try
             {
-                for (int i = 1; i <= numeroB; i++)
-                    acum += numeroA;
+                if (numeroA > numeroB)
+                {
+                    for (int i = 1; i <= numeroB; i++)
+                        acum += numeroA;
+                }
+                else
+                    for (int i = 1; i <= numeroA; i++)
+                        acum += numeroB;
+                return acum;
             }
-            else
-                for (int i = 1; i <= numeroA; i++)
-                    acum += numeroB;
+            catch (Exception ex)
+            {
 
-            return acum;
+                throw ex;
+            }
         }
 
         public double ValorMayorConUnFor(double[] valores)
         {
             double mayor = valores[0];
-            for (int i = 1; i < valores.Length; i++)
+            try
             {
-                if (valores[i] > mayor)
-                    mayor = valores[i];
-            }
+                for (int i = 1; i < valores.Length; i++)
+                {
+                    if (valores[i] > mayor)
+                        mayor = valores[i];
+                }
 
-            return mayor;
+                return mayor;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public IList<string> SplitCadena(string texto)
@@ -39,58 +53,93 @@ namespace ADM.PruebaTecnica.Services.Domain
             IList<string> palabras = new List<string>();
             string cadena = string.Empty;
 
-            for (int i = 0; i < texto.Length; i++)
+            try
             {
-                string letra = texto[i].ToString();
-                if (letra != " ")
-                    cadena += letra;
-                if (i == texto.Length - 1)
-                    palabras.Add(cadena);
-                if (letra == " ")
+                for (int i = 0; i < texto.Length; i++)
                 {
-                    palabras.Add(cadena);
-                    cadena = string.Empty;
+                    string letra = texto[i].ToString();
+                    if (letra != " ")
+                        cadena += letra;
+                    if (i == texto.Length - 1)
+                        palabras.Add(cadena);
+                    if (letra == " ")
+                    {
+                        palabras.Add(cadena);
+                        cadena = string.Empty;
+                    }
                 }
+                return palabras;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
             }
 
-            return palabras;
         }
 
         public int VecesPalabraEnTexto(IList<string> palabras, string palabra)
         {
             int count = 0;
 
-            for (int i = 0; i < palabras.Count; i++)
+            try
             {
-                if (palabras[i] == palabra)
-                    count += 1;
-            }
+                for (int i = 0; i < palabras.Count; i++)
+                {
+                    if (palabras[i] == palabra)
+                        count += 1;
+                }
 
-            return count;
+                return count;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
         public string InvertirCadena(string palabra)
         {
             string cadena = string.Empty;
-            for (int i = palabra.Length - 1; i >= 0; i--)
-                cadena += palabra[i];
+            try
+            {
+                for (int i = palabra.Length - 1; i >= 0; i--)
+                    cadena += palabra[i];
 
-            return cadena;
+                return cadena;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
-        public string QuitarTildes(string palabra) 
+        public string QuitarTildes(string palabra)
         {
             string caracteresEspeciales = "áéíóúñ";
-            palabra = Regex.Replace(caracteresEspeciales.Normalize(NormalizationForm.FormD), @"[^a-zA-z0-9 ]+", palabra);
-            return palabra;
+            try
+            {
+                palabra = Regex.Replace(caracteresEspeciales.Normalize(NormalizationForm.FormD), @"[^a-zA-z0-9 ]+", palabra);
+                return palabra;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
 
-        public bool Palindroma(string palabra, string palabraInversa) 
+        public bool Palindroma(string palabra, string palabraInversa)
         {
             bool palindroma = false;
-            if (palabra.Equals(palabraInversa))
-                palindroma = !palindroma;
-            return palindroma;
+            try
+            {
+                if (palabra.Equals(palabraInversa))
+                    palindroma = !palindroma;
+                return palindroma;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
